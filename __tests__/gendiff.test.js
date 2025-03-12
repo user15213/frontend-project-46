010f594f31062cef2ef9d38d029e95f10995a68f
user15213 <resomakar@gmail.com>
2025-03-13 01:25:35 +0500

    add yml files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import mockfs from 'mock-fs';
 import { parseFile } from '../parse';
+import yaml from 'js-yaml';
 
 describe('parseFile', () => {
   beforeAll(() => {
@@ -9,6 +10,10 @@ describe('parseFile', () => {
       __fixtures__: {
         'validFile.json': JSON.stringify({ key1: 'value1', key2: 'value2' }),
         'invalidFile.json': '{ key1: value1, key2: value2 }',
+
+        'validFile.yml': yaml.dump({ key1: 'value1', key2: 'value2' }),
+        'validFile.yaml': yaml.dump({ key1: 'value1', key2: 'value2' }),
+        'invalidFile.yml': 'key1: value1 key2: value2',
       },
     });
   });
@@ -25,6 +30,25 @@ describe('parseFile', () => {
 
   it('should throw an error if the file content is not valid JSON', () => {
     const filePath = path.resolve('__fixtures__', 'invalidFile.json');
+    expect(() => parseFile(filePath)).toThrowError(
+      /Error reading or parsing file/
+    );
+  });
+
+  it('should parse a valid YAML file (.yml) correctly', () => {
+    const filePath = path.resolve('__fixtures__', 'validFile.yml');
+    const result = parseFile(filePath);
+    expect(result).toEqual({ key1: 'value1', key2: 'value2' });
+  });
+
+  it('should parse a valid YAML file (.yaml) correctly', () => {
+    const filePath = path.resolve('__fixtures__', 'validFile.yaml');
+    const result = parseFile(filePath);
+    expect(result).toEqual({ key1: 'value1', key2: 'value2' });
+  });
+
+  it('should throw an error if the YAML file content is not valid', () => {
+    const filePath = path.resolve('__fixtures__', 'invalidFile.yml');
     expect(() => parseFile(filePath)).toThrowError(
       /Error reading or parsing file/
     );
